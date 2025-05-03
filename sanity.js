7bba9a083ec554a01f75ea0fcf7fd129011f91f6
Dhiraj Holkar <dhirajspock2121@gmail.com>
2025-05-03 19:22:20 +0530

    hailee

--- sanity.js
+++ sanity.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: 'w9h9dpl8',  // Replace with your actual Sanity Project ID
+  projectId: 'owumx8en',  // Replace with your actual Sanity Project ID
   dataset: 'production',  // Replace with your actual Sanity Dataset
   useCdn: false,
   apiVersion: '2023-12-01',
